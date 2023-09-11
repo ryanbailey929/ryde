c0c9bfaccd4a4f678b92d7cd36661edf5caa0a90
Ryan Bailey <ryancbailey3500@gmail.com>
2023-09-11 15:46:58 +0100

    Implemented Widget.

--- src/base/widget.cpp
+++ src/base/widget.cpp
@@ -2,7 +2,80 @@
 
 #include "widget.hpp"
 
-Widget::Widget()
-{
+#include <glm/glm.hpp>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <string>
+
+unsigned int Widget::create_shader(std::string shader_source, ShaderType shader_type)
+{
+    unsigned int shader;
+    if(shader_type == ShaderType::VERTEX)
+        shader = glCreateShader(GL_VERTEX_SHADER);
+    else //shader_type == ShaderType::FRAGMENT
+        shader = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* shader_source_c_str {shader_source.c_str()};
+    glShaderSource(shader, 1, &shader_source_c_str, nullptr);
+    glCompileShader(shader);
+    compile_check(shader, shader_type);
+    return shader;
+}
+
+unsigned int Widget::create_program(unsigned int vertex_shader,
+                                    unsigned int fragment_shader)
+{
+    unsigned int shader_program {glCreateProgram()};
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+    link_check(shader_program);
+    return shader_program;
+}
+
+void Widget::compile_check(unsigned int shader, ShaderType shader_type)
+{
+    char info_log[512];
+    int success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(shader, 512, nullptr, info_log);
+        if(shader_type == ShaderType::VERTEX)
+            std::cerr << "Vertex";
+        else //shader_type == ShaderType::FRAGMENT
+            std::cerr << "Fragment";
+        std::cerr << " shader compilation failed. Details: " << info_log << '\n';
+        throw -1;
+    }
+}
+
+void Widget::link_check(unsigned int shader_program)
+{
+    char info_log[512];
+    int success;
+    glGetShaderiv(shader_program, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(shader_program, 512, nullptr, info_log);
+        std::cerr << "Shader program linking failed. Details: " << info_log << '\n';
+        throw -1;
+    }
+}
+
+float* create_quad_vertices(float x, float y, float width, float height,
+                                   float z)
+{
+    return new float[12] { x,         y,          z,
+                           x + width, y,          z,
+                           x + width, y + height, z,
+                           x,         y + height, z};
+}
+
+unsigned int* create_quad_indices()
+{
+    return new unsigned int[6] {0, 1, 2,
+                                1, 3, 2};
 }
--- src/base/widget.hpp
+++ src/base/widget.hpp
@@ -3,10 +3,41 @@
 #ifndef RYDE_BASE_WIDGET_HPP
 #define RYDE_BASE_WIDGET_HPP
 
+#include <glm/glm.hpp>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <string>
+
+enum class ShaderType
+{
+    VERTEX,
+    FRAGMENT
+};
+
 class Widget
 {
+    glm::mat4 model {glm::mat4(1.0f)};
 public:
-    Widget();
+    Widget()=default;
+    virtual void draw()=0;
+protected:
+    static unsigned int create_shader(std::string shader_source,
+                                      ShaderType shader_type);
+    static unsigned int create_program(unsigned int vertex_shader,
+                                       unsigned int fragment_shader);
+    static void compile_check(unsigned int shader, ShaderType shader_type);
+    static void link_check(unsigned int shader_program);
+
+    //will produce the 4 vertices (12 floats), needed to create a quad defined by x, y,
+    //width and height at the given z. Quad is in the x-y plane, obviously.
+    static float* create_quad_vertices(float x, float y, float width, float height,
+                                       float z);
+    //will produce the 6 indices (6 unsigned ints) for the vertices produced by
+    //create_quad_vertices.
+    static unsigned int* create_quad_indices();
 };
 
 #endif
